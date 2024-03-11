fc4c96d7af84ae1fd77b9c282a53865eb2119d8b
kalan <kalansoft@gmail.com>
2024-03-11 20:46:08 +0700

    fix

--- substrate/run.sh
+++ substrate/run.sh
@@ -11,7 +11,7 @@ echo "Starting deitos-gate ..."
 export DG_PORT="9090" 
 export DEITOS_IP="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
 export DEITOS_NODE="ws://127.0.0.1:9944"
-export HDFS_URI="http://127.0.0.1:50070"
+export HDFS_URI="http://master.deitos.network:50070"
 /node-template/deitos-gate/target/release/deitos-gate &
 
 
