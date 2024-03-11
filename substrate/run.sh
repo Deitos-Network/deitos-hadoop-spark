8d01f854f58f70e67d42b9677315b35f3b1e9cca
Alexander Kalankhodzhaev <kalansoft@gmail.com>
2024-03-11 17:04:29 +0700

    set ALICE as IP

--- substrate/run.sh
+++ substrate/run.sh
@@ -9,7 +9,7 @@ export HDFS_URI="http://master.deitos.network:50070/webhdfs/v1/data/deitos"
 echo "Starting deitos-gate ..."
 
 export DG_PORT="9090" 
-export DEITOS_IP="127.0.0.1"
+export DEITOS_IP="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
 export DEITOS_NODE="ws://127.0.0.1:9944"
 export HDFS_URI="http://127.0.0.1:50070"
 /node-template/deitos-gate/target/release/deitos-gate &
